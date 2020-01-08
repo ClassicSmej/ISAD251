3f7e4c1d4a5d91b41118ec79db03c7dedc8b5fbf
James White <44773230+jwhite96@users.noreply.github.com>
2020-01-08 00:29:13 +0000

    Update dbFunctions.php

--- public/dbFunctions.php
+++ public/dbFunctions.php
@@ -5,9 +5,8 @@ session_start();
 
 $db = new dbContext();
 
-
-//removing item
-$productID = $_REQUEST['REMOVE'];
+//removing item from menu
+$productID = $_REQUEST['OFFSALE'];
 
 $sql = "UPDATE products SET Status = 'Not on Sale' WHERE ProductID ='$productID'";
 $statement = $db->connection->prepare($sql);
@@ -17,7 +16,7 @@ header("Location: admin.php");
 
 
 //adding item to menu
-$productID = $_REQUEST['PUSH'];
+$productID = $_REQUEST['ONSALE'];
 
 $sql = "UPDATE products SET Status = 'On Sale' WHERE ProductID ='$productID'";
 $statement = $db->connection->prepare($sql);
@@ -25,29 +24,31 @@ $statement->execute();
 
 header("Location: admin.php");
 
+
 //adding new item
-$name = $_REQUEST['NAME'];
-$description = $_REQUEST['DESCRIPTION'];
-$price = $_REQUEST['PRICE'];
-$category = $_REQUEST['CATEGORY'];
-$status = $_REQUEST['STATUS'];
+$newName = $_REQUEST['NEWNAME'];
+$newDescription = $_REQUEST['NEWDESCRIPTION'];
+$newPrice = $_REQUEST['NEWPRICE'];
+$newCategory = $_REQUEST['NEWCATEGORY'];
+$newStatus = $_REQUEST['NEWSTATUS'];
 
 $sql = "INSERT INTO products(Name, Description, Price, Category, Status)
-VALUES ('$name', '$description', '$price', '$category', '$status')";
+VALUES ('$newName', '$newDescription', '$newPrice', '$newCategory', '$newStatus')";
 
 $statement = $db->connection->prepare($sql);
 $statement->execute();
 
 header("Location: admin.php");
 
-//editing an item
-$editName = $_REQUEST['EDITNAME'];
-$editDes = $_REQUEST['EDITDES'];
-$editPrice = $_REQUEST['EDITPRICE'];
-$editCat = $_REQUEST['EDITCAT'];
-$editStatus = $_REQUEST['EDITSTATUS'];
 
-$sql = "UPDATE products SET Name = '$editName', Description = '$editDes', Price = '$editPrice', Category = '$editCat', Status = '$editStatus' WHERE ProductID ='$productID'";
+//editing an item
+$ID = $_REQUEST['EDIT'];
+$name = $_REQUEST['NAME'];
+$description = $_REQUEST['DESCRIPTION'];
+$price = $_REQUEST['PRICE'];
+$category = $_REQUEST['CATEGORY'];
+
+$sql = "UPDATE products SET Name = '$name', Description = '$description', Price = '$price', Category = '$category' WHERE ProductID ='$ID'";
 
 $statement = $db->connection->prepare($sql);
 $statement->execute();

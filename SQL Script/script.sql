c8534cf50c709ae7473301e34a0a00656004076d
James White <44773230+jwhite96@users.noreply.github.com>
2020-01-09 21:16:39 +0000

    Update script.sql

--- SQL Script/script.sql
+++ SQL Script/script.sql
@@ -44,3 +44,26 @@ from ((`isad251_jwhite`.`orders` join `isad251_jwhite`.`products`)
 where ((`isad251_jwhite`.`orderitems`.`OrderID` = `isad251_jwhite`.`orders`.`OrderID`) and
        (`isad251_jwhite`.`orderitems`.`ProductID` = `isad251_jwhite`.`products`.`ProductID`))
 group by `isad251_jwhite`.`orderitems`.`ItemID`;
+
+CREATE PROCEDURE `order_view`()
+BEGIN
+CREATE VIEW `isad251_jwhite`.`orderdetails` AS
+    SELECT 
+        `isad251_jwhite`.`orderitems`.`ItemID` AS `ItemID`,
+        `isad251_jwhite`.`orders`.`OrderID` AS `OrderID`,
+        `isad251_jwhite`.`orders`.`OrderDate` AS `OrderDate`,
+        `isad251_jwhite`.`orderitems`.`ProductID` AS `ProductID`,
+        `isad251_jwhite`.`products`.`Name` AS `Name`,
+        `isad251_jwhite`.`orderitems`.`Quantity` AS `Quantity`,
+        SUM((`isad251_jwhite`.`orderitems`.`Quantity` * `isad251_jwhite`.`products`.`Price`)) AS `TotalCost`
+    FROM
+        ((`isad251_jwhite`.`orders`
+        JOIN `isad251_jwhite`.`products`)
+        JOIN `isad251_jwhite`.`orderitems`)
+    WHERE
+        ((`isad251_jwhite`.`orderitems`.`OrderID` = `isad251_jwhite`.`orders`.`OrderID`)
+            AND (`isad251_jwhite`.`orderitems`.`ProductID` = `isad251_jwhite`.`products`.`ProductID`))
+    GROUP BY `isad251_jwhite`.`orderitems`.`ItemID`;
+END
+
+
